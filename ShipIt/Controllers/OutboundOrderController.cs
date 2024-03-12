f09d1566862507d1553d7b3f58b75e2e1a77babf
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-03-12 22:00:56 +0000

    feat: Loading products into trucks in Outbound Order

--- ShipIt/Controllers/OutboundOrderController.cs
+++ ShipIt/Controllers/OutboundOrderController.cs
@@ -1,8 +1,10 @@
 ﻿﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Hosting;
 using ShipIt.Exceptions;
 using ShipIt.Models.ApiModels;
 using ShipIt.Repositories;
@@ -68,7 +70,7 @@ namespace ShipIt.Controllers
                     lineItems.Add(new StockAlteration(product.Id, orderLine.quantity));
                     lineItemsWithWeights.Add(
                         new StockAlterationWithWeights(
-                            product.Id,
+                            product.Gtin,
                             orderLine.quantity,
                             product.Weight
                         )
@@ -117,6 +119,9 @@ namespace ShipIt.Controllers
 
             float totalOrderWeightInKgs = 0;
             int numberOfTrucks = 0;
+            var trucks = new List<Truck>();
+
+            //{product.id, toalWeight}
 
             if (errors.Count > 0)
             {
@@ -124,21 +129,166 @@ namespace ShipIt.Controllers
             }
             else
             {
+                //lineItemsWithWeights<Gtin,quantity and weight>
+                // var TrucksLookup = new Dictionary<int, Truck>();
+
+                var truckNum = 0;
                 totalOrderWeightInKgs =
                     lineItemsWithWeights.Sum(item => item.Weight * item.Quantity) / 1000;
                 numberOfTrucks = (int)Math.Ceiling(totalOrderWeightInKgs / 2000);
+
+                float truckWeight = 0;
+                var productsInTruck = new List<ProductInTruck>();
+
+                //What to do if trucks has weight left?[1500, 1800, 500, 100]
+                //What if ProductWeight>2000?
+                //First Truck
+                Truck currentTruck = new Truck
+                {
+                    TruckNumber = ++truckNum,
+                    WeightInTruck = 0,
+                    ProductsList = new List<ProductInTruck>()
+                };
+                float MaxTruckWeight = 2000;
+                
+                
+                for (int i = 0; i < lineItemsWithWeights.Count; i++)
+                {
+                    var item = lineItemsWithWeights[i];
+                    var product = products[item.Gtin];
+                    var weightInKgsPerProduct = product.Weight / 1000;
+                    var quantity = item.Quantity;
+                    //truck=1800
+                    //produc=400
+                    do
+                    {
+                        
+                        Console.WriteLine($"**** Quantity {quantity}");
+                        var productTotalWeight = (quantity * product.Weight) / 1000;
+
+                        if (productTotalWeight > MaxTruckWeight)
+                        {
+                            //DoSomething
+                        }
+                        // truckWeight = currentTruck.WeightInTruck + productTotalWeight;
+                        var WeightAvailable = MaxTruckWeight - currentTruck.WeightInTruck;
+                        if (WeightAvailable >= productTotalWeight)
+                        {
+                            Console.WriteLine($"Weight Available: {WeightAvailable}");
+                            Console.WriteLine($"---->{truckWeight}");
+                            var currProduct = new ProductInTruck
+                            {
+                                ProductId = product.Id,
+                                ProductName = product.Name,
+                                Quantity = quantity,
+                                TotalWeight = quantity * weightInKgsPerProduct,
+                            };
+                            currentTruck.WeightInTruck += productTotalWeight;
+                            currentTruck.ProductsList.Add(currProduct);
+                            currentTruck.ProductsList.ForEach(product =>
+                            {
+                                Console.WriteLine(product);
+                            });
+                            quantity = 0;
+                        }
+                        else if (WeightAvailable >= weightInKgsPerProduct)
+                        {
+                            Console.WriteLine($"---->WeightAvailable: {WeightAvailable}");
+                            Console.WriteLine(
+                                $"---->weightInKgsPerProduct:{weightInKgsPerProduct}"
+                            );
+
+                            //Following is to calculate how many products that can be fit in the available space in truck
+                            var quantityFitInTruck = (int)
+                                Math.Floor(WeightAvailable / weightInKgsPerProduct);
+                            var quantityWeight = quantityFitInTruck * weightInKgsPerProduct;
+                            var currProduct = new ProductInTruck
+                            {
+                                ProductId = product.Id,
+                                ProductName = product.Name,
+                                Quantity = quantityFitInTruck,
+                                TotalWeight = quantityWeight,
+                            };
+                            quantity = quantity - quantityFitInTruck;
+                            currentTruck.ProductsList.Add(currProduct);
+                            currentTruck.WeightInTruck += quantityWeight;
+                            WeightAvailable -= quantityWeight;
+                            Console.WriteLine($"QuantityThatFitInTruck: {quantityFitInTruck}");
+                            Console.WriteLine($"quantityWeight: {quantityWeight}");
+                            
+                        }
+                        if (WeightAvailable < weightInKgsPerProduct || quantity==0)
+                        {
+                            Console.WriteLine("TruckAdded");
+                            trucks.Add(currentTruck);
+                            currentTruck = new Truck
+                            {
+                                TruckNumber = ++truckNum,
+                                WeightInTruck = 0,
+                                ProductsList = new List<ProductInTruck>()
+                            };
+                            truckWeight = 0;
+                        }
+                        
+                    } while (quantity > 0);
+                }
             }
 
             _stockRepository.RemoveStock(request.WarehouseId, lineItems);
             return new OutboundOrderResponseModel()
             {
                 TotalOrderWeightInKgs = totalOrderWeightInKgs,
-                NumberOfTrucks = numberOfTrucks
+                NumberOfTrucks = numberOfTrucks,
+                Truck = trucks
             };
         }
     }
 }
 
+// while (lineItemsWithWeights.Count() > 0)
+// {
+//     float truckWeight = 0;
+//     var productsInTruck = new List<ProductInTruck>();
+//     //If There are no trucks
+//     if (trucks.Count == 0)
+//     {
+//         var newTruck = new Truck()
+//         {
+//             TruckNumber = truckNum,
+//             WeightInTruck = 0,
+//             ProductsList = new List<ProductInTruck>()
+//         };
+
+//         trucks.Add(newTruck);
+//         TrucksLookup[truckNum]=newTruck;
+//     }
+//     foreach (var item in lineItemsWithWeights) //[1500, 1800, 500, 100]
+//     {
+//         var product = products[item.Gtin];
+//         var productWeight = (item.Quantity * product.Weight) / 1000;
+//         var currProduct=  new ProductInTruck
+//                     {
+//                         ProductId = product.Id,
+//                         ProductName = product.Name,
+//                         Quantity = item.Quantity,
+//                         TotalWeight = productWeight,
+//                     };
+//         if (productWeight > 2000) {
+//             var quantity=item.Quantity;
+//             //Do Something
+//          }
+//          bool ProductAdded = false;
+//          for(int i =0;i<trucks.Count&&!ProductAdded;i++){
+//             var currTruck=trucks[i];
+//             var weightLeft = 2000-currTruck.WeightInTruck;
+//             if(weightLeft>=productWeight){
+//                 currTruck.ProductsList.Add(currProduct);
+//                 lineItemsWithWeights.Remove(item);
+
+//             }
+//          }
+//     }
+// }
 //SELECT * FROM public.stock as s
 //Join public.gtin as g ON s.p_id=g.p_id WHERE s.w_id=9 Order By g.m_g desc;
 
--- ShipIt/Models/ApiModels/OutboundOrderResponseModel.cs
+++ ShipIt/Models/ApiModels/OutboundOrderResponseModel.cs
@@ -11,6 +11,7 @@ namespace ShipIt.Models.ApiModels
     {
         public float TotalOrderWeightInKgs { get; set; }
         public int NumberOfTrucks { get; set; }
+        public List<Truck> Truck { get; set; }
 
         public override String ToString()
         {
@@ -23,13 +24,13 @@ namespace ShipIt.Models.ApiModels
 
     public class StockAlterationWithWeights
     {
-        public int ProductId { get; set; }
+        public string Gtin { get; set; }
         public float Weight { get; set; }
         public int Quantity { get; set; }
 
-        public StockAlterationWithWeights(int productId, int quantity, float weight)
+        public StockAlterationWithWeights(string gtin, int quantity, float weight)
         {
-            this.ProductId = productId;
+            this.Gtin = gtin;
             this.Quantity = quantity;
             this.Weight = weight;
 
@@ -39,4 +40,57 @@ namespace ShipIt.Models.ApiModels
             }
         }
     }
+
+    public class Truck
+    {
+        public int TruckNumber { get; set; }
+        public float WeightInTruck { get; set; }
+        public List<ProductInTruck> ProductsList { get; set; } = [];
+
+        public override String ToString()
+        {
+            return new StringBuilder()
+                .AppendFormat("TruckNumber: {0}, ", TruckNumber)
+                .AppendFormat("WeightInTruck: {0}", WeightInTruck)
+                .ToString();
+        }
+    }
+
+    public class ProductInTruck
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public float TotalWeight { get; set; }
+
+        public override String ToString()
+        {
+            return new StringBuilder()
+                .AppendFormat("ProductId: {0}, ", ProductId)
+                .AppendFormat("\t ProductName: {0}", ProductName)
+                .AppendFormat("\t Quantity: {0}", Quantity)
+                .AppendFormat("\t TotalWeight: {0}", TotalWeight)
+                .ToString();
+        }
+    }
 }
+
+//product:800kgs; 7000kg, 4
+/*
+    {truckNumber1:2000-1500, truckNumber2:2000-1500, truckNumber3:2000-1500, truckNumber4:2000-1500}
+    */
+/*
+Truck : 1
+WeightInTruck : 1800Kgs
+
+
+Truck : 2
+WeightInTruck : 1900Kgs
+
+
+Truck : 3
+WeightInTruck : 1500Kgs var minDiffer = 2000; 2000-WightInTruck< minDiffer, minDiffer = 2000-WightInTruck,
+
+Truck : 4
+WeightInTruck : 1600Kgs
+*/
