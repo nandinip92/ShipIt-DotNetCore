50abf2f266e49d2f3d3839209118b23b8d5ef1a3
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-03-12 22:22:50 +0000

    Outbound order trucks

--- ShipIt/Controllers/OutboundOrderController.cs
+++ ShipIt/Controllers/OutboundOrderController.cs
@@ -129,19 +129,14 @@ namespace ShipIt.Controllers
             }
             else
             {
-                //lineItemsWithWeights<Gtin,quantity and weight>
-                // var TrucksLookup = new Dictionary<int, Truck>();
-
                 var truckNum = 0;
                 totalOrderWeightInKgs =
                     lineItemsWithWeights.Sum(item => item.Weight * item.Quantity) / 1000;
                 numberOfTrucks = (int)Math.Ceiling(totalOrderWeightInKgs / 2000);
 
-                float truckWeight = 0;
-                var productsInTruck = new List<ProductInTruck>();
-
                 //What to do if trucks has weight left?[1500, 1800, 500, 100]
                 //What if ProductWeight>2000?
+
                 //First Truck
                 Truck currentTruck = new Truck
                 {
@@ -150,32 +145,50 @@ namespace ShipIt.Controllers
                     ProductsList = new List<ProductInTruck>()
                 };
                 float MaxTruckWeight = 2000;
-                
-                
+                if (trucks.Count == 0)
+                {
+                    trucks.Add(currentTruck);
+                }
+
                 for (int i = 0; i < lineItemsWithWeights.Count; i++)
                 {
                     var item = lineItemsWithWeights[i];
                     var product = products[item.Gtin];
                     var weightInKgsPerProduct = product.Weight / 1000;
                     var quantity = item.Quantity;
-                    //truck=1800
-                    //produc=400
                     do
                     {
-                        
-                        Console.WriteLine($"**** Quantity {quantity}");
+                        foreach (var truck in trucks)
+                        {
+                            if (MaxTruckWeight - truck.WeightInTruck > weightInKgsPerProduct)
+                            {
+                                currentTruck = truck;
+                                break;
+                            }
+                            else
+                            {
+                                currentTruck = null;
+                            }
+                        }
+                        if (currentTruck == null)
+                        {
+                            currentTruck = new Truck
+                            {
+                                TruckNumber = ++truckNum,
+                                WeightInTruck = 0,
+                                ProductsList = new List<ProductInTruck>()
+                            };
+                            trucks.Add(currentTruck);
+                        }
+                        // Console.WriteLine($"**** Quantity {quantity}");
                         var productTotalWeight = (quantity * product.Weight) / 1000;
 
-                        if (productTotalWeight > MaxTruckWeight)
-                        {
-                            //DoSomething
-                        }
                         // truckWeight = currentTruck.WeightInTruck + productTotalWeight;
                         var WeightAvailable = MaxTruckWeight - currentTruck.WeightInTruck;
                         if (WeightAvailable >= productTotalWeight)
                         {
-                            Console.WriteLine($"Weight Available: {WeightAvailable}");
-                            Console.WriteLine($"---->{truckWeight}");
+                            // Console.WriteLine($"Weight Available: {WeightAvailable}");
+                            // Console.WriteLine($"---->{truckWeight}");
                             var currProduct = new ProductInTruck
                             {
                                 ProductId = product.Id,
@@ -185,18 +198,14 @@ namespace ShipIt.Controllers
                             };
                             currentTruck.WeightInTruck += productTotalWeight;
                             currentTruck.ProductsList.Add(currProduct);
-                            currentTruck.ProductsList.ForEach(product =>
-                            {
-                                Console.WriteLine(product);
-                            });
                             quantity = 0;
                         }
                         else if (WeightAvailable >= weightInKgsPerProduct)
                         {
-                            Console.WriteLine($"---->WeightAvailable: {WeightAvailable}");
-                            Console.WriteLine(
-                                $"---->weightInKgsPerProduct:{weightInKgsPerProduct}"
-                            );
+                            // Console.WriteLine($"---->WeightAvailable: {WeightAvailable}");
+                            // Console.WriteLine(
+                            //     $"---->weightInKgsPerProduct:{weightInKgsPerProduct}"
+                            // );
 
                             //Following is to calculate how many products that can be fit in the available space in truck
                             var quantityFitInTruck = (int)
@@ -213,23 +222,9 @@ namespace ShipIt.Controllers
                             currentTruck.ProductsList.Add(currProduct);
                             currentTruck.WeightInTruck += quantityWeight;
                             WeightAvailable -= quantityWeight;
-                            Console.WriteLine($"QuantityThatFitInTruck: {quantityFitInTruck}");
-                            Console.WriteLine($"quantityWeight: {quantityWeight}");
-                            
+                            // Console.WriteLine($"QuantityThatFitInTruck: {quantityFitInTruck}");
+                            // Console.WriteLine($"quantityWeight: {quantityWeight}");
                         }
-                        if (WeightAvailable < weightInKgsPerProduct || quantity==0)
-                        {
-                            Console.WriteLine("TruckAdded");
-                            trucks.Add(currentTruck);
-                            currentTruck = new Truck
-                            {
-                                TruckNumber = ++truckNum,
-                                WeightInTruck = 0,
-                                ProductsList = new List<ProductInTruck>()
-                            };
-                            truckWeight = 0;
-                        }
-                        
                     } while (quantity > 0);
                 }
             }
@@ -244,119 +239,3 @@ namespace ShipIt.Controllers
         }
     }
 }
-
-// while (lineItemsWithWeights.Count() > 0)
-// {
-//     float truckWeight = 0;
-//     var productsInTruck = new List<ProductInTruck>();
-//     //If There are no trucks
-//     if (trucks.Count == 0)
-//     {
-//         var newTruck = new Truck()
-//         {
-//             TruckNumber = truckNum,
-//             WeightInTruck = 0,
-//             ProductsList = new List<ProductInTruck>()
-//         };
-
-//         trucks.Add(newTruck);
-//         TrucksLookup[truckNum]=newTruck;
-//     }
-//     foreach (var item in lineItemsWithWeights) //[1500, 1800, 500, 100]
-//     {
-//         var product = products[item.Gtin];
-//         var productWeight = (item.Quantity * product.Weight) / 1000;
-//         var currProduct=  new ProductInTruck
-//                     {
-//                         ProductId = product.Id,
-//                         ProductName = product.Name,
-//                         Quantity = item.Quantity,
-//                         TotalWeight = productWeight,
-//                     };
-//         if (productWeight > 2000) {
-//             var quantity=item.Quantity;
-//             //Do Something
-//          }
-//          bool ProductAdded = false;
-//          for(int i =0;i<trucks.Count&&!ProductAdded;i++){
-//             var currTruck=trucks[i];
-//             var weightLeft = 2000-currTruck.WeightInTruck;
-//             if(weightLeft>=productWeight){
-//                 currTruck.ProductsList.Add(currProduct);
-//                 lineItemsWithWeights.Remove(item);
-
-//             }
-//          }
-//     }
-// }
-//SELECT * FROM public.stock as s
-//Join public.gtin as g ON s.p_id=g.p_id WHERE s.w_id=9 Order By g.m_g desc;
-
-//POST : OutboundOrderRequestModel
-// {
-// "WarehouseId" : 9,
-// "OrderLines" : [
-//                     {
-//                         "gtin" : "0008336884202",
-//                         "quantity" : 40
-//                     },
-
-//                     {
-//                         "gtin" : "0008338484202",
-//                         "quantity" : 29
-//                     },
-
-//                     {
-//                         "gtin" : "0008346019908",
-//                         "quantity" :50
-//                     },
-
-//                     {
-//                         "gtin" : "0008346036004",
-//                         "quantity" : 35
-//                     },
-//                     {
-//                         "gtin" : "0024617180320",
-//                         "quantity" : 35
-//                     },
-//                     {
-//                         "gtin" : "0079100216901",
-//                         "quantity" : 28
-//                     },
-//                     {
-//                         "gtin" : "0730521100209",
-//                         "quantity" : 31
-//                     }
-// ]
-// }
-
-
-// {
-// "WarehouseId" : 9,
-// "OrderLines" : [
-//                     {
-//                         "gtin" : "0049022327474",
-//                         "quantity" : 4
-//                     },
-
-//                     {
-//                         "gtin" : "0052649777782",
-//                         "quantity" : 9
-//                     },
-
-//                     {
-//                         "gtin" : "0052742462608",
-//                         "quantity" :2
-//                     },
-
-//                     {
-//                         "gtin" : "0022000115706",
-//                         "quantity" : 5
-//                     },
-//                     {
-//                         "gtin" : "7896262301121",
-//                         "quantity" : 3
-//                     }
-
-// ]
-// }
